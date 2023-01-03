03b9f9d5edcc812a6d92707e30e26cbc507fdc5c
SimchaTeich <sim.teich@gmail.com>
2023-01-03 19:34:15 +0200

    build the skeleton for the first function

--- src/src/Ex2/Ex2_1.java
+++ src/src/Ex2/Ex2_1.java
@@ -1,10 +1,27 @@
 package Ex2;
 
+import java.util.Random;
+
 public class Ex2_1
 {
     public static String[] createTextFiles(int n, int seed, int bound)
     {
-        //TODO
+        // create random generator for number of lines in each file
+        Random rand = new Random(seed);
+
+        // create n file
+        for(int i = 0; i < n; i++)
+        {
+            int numOfLines = rand.nextInt(bound);
+
+            // TODO: create new file
+
+            for(int j = 0; j < numOfLines; j++)
+            {
+                // TODO: append new line to the last file
+            }
+
+        }
     }
 
     public static int getNumOfLines(String[] fileNames)
