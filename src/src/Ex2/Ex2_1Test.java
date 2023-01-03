33afcab01a32922e612ba23e96df904c67a06ec5
Sari <sygw6457@gmail.com>
2023-01-03 20:07:29 +0200

    add test class

---
+++ src/src/Ex2/Ex2_1Test.java
@@ -0,0 +1,16 @@
+package Ex2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Ex2_1Test {
+
+    @Test
+    void createTextFiles() {
+    }
+
+    @Test
+    void getNumOfLines() {
+    }
+}
