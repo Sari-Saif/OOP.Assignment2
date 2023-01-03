be9bff0fdc3e5325c891f8ba0f99ebb02e054485
Sari <sygw6457@gmail.com>
2023-01-03 19:47:19 +0200

    public static int getNumOfLines(String[] fileNames) -> Done

--- src/src/Ex2/Ex2_1.java
+++ src/src/Ex2/Ex2_1.java
@@ -1,5 +1,8 @@
 package Ex2;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.util.Random;
 
 public class Ex2_1
@@ -29,9 +32,22 @@ public class Ex2_1
     public static int getNumOfLines(String[] fileNames)
     {
         //TODO
+        // initial number for lines
         int NumOfLines = 0 ;
+        for( int i =0 ; i < fileNames.length ; i++)
+        {
+           try {
+               // we get the name of file 'i'
+               String name_Of_file = fileNames[i];
+               // read the specific file
+               BufferedReader fileReader = new BufferedReader(new FileReader(name_Of_file));
+               // get number of files by streaming .
+               NumOfLines += fileReader.lines().count();
+           }
+           catch (Exception e){}
 
-
+        }
+        return NumOfLines;
     }
 
     public int getNumOfLinesThreads(String[] fileNames)
