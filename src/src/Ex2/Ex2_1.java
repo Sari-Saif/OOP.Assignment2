a65974239badf9125c5305aa64fb8896ca1085d3
SimchaTeich <sim.teich@gmail.com>
2023-01-03 19:50:47 +0200

    implement createTextFiles function

--- src/src/Ex2/Ex2_1.java
+++ src/src/Ex2/Ex2_1.java
@@ -1,29 +1,42 @@
 package Ex2;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.util.Random;
 
 public class Ex2_1
 {
     public static String[] createTextFiles(int n, int seed, int bound)
     {
+        String[] textFileNames = new String[n];
+
         // create random generator for number of lines in each file
         Random rand = new Random(seed);
 
-        // create n file
+        // create n files
         for(int i = 0; i < n; i++)
         {
             int numOfLines = rand.nextInt(bound);
 
-            // TODO: create new file
-
-            for(int j = 0; j < numOfLines; j++)
+            try
             {
-                // TODO: append new line to the last file
-            }
+                // create new file
+                FileWriter fileWriter_i = new FileWriter("file_" + i + ".txt");
 
-            // todo: delete this line
+                // append random nunber of lines to the file.
+                for(int j = 0; j < numOfLines; j++)
+                {
+                    fileWriter_i.write("Line number " + j+1 + "\n");
+                }
 
+                // save the file name.
+                textFileNames[i] = "file_" + i + ".txt";
+
+            }catch (Exception e){}
         }
+
+        return textFileNames;
     }
 
     public static int getNumOfLines(String[] fileNames)
