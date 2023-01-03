14f774947a6deef44b1db05d41bde9c4a12c5596
SimchaTeich <sim.teich@gmail.com>
2023-01-03 19:30:14 +0200

    append the needed signature of functions for this class

--- src/src/Ex2/Ex2_1.java
+++ src/src/Ex2/Ex2_1.java
@@ -1,4 +1,24 @@
 package Ex2;
 
-public class Ex2_1 {
+public class Ex2_1
+{
+    public static String[] createTextFiles(int n, int seed, int bound)
+    {
+        //TODO
+    }
+
+    public static int getNumOfLines(String[] fileNames)
+    {
+        //TODO
+    }
+
+    public int getNumOfLinesThreads(String[] fileNames)
+    {
+        //TODO
+    }
+
+    public int getNumOfLinesThreadPool(String[] fileNames)
+    {
+        //TODO
+    }
 }
