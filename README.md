393bf41b615e93999379b65c028353e683b54540
Sari <sygw6457@gmail.com>
2023-01-04 00:31:57 +0200

    update the tables in README.md

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # OOP.Assignment2
+
+# Scenario 1 : Up to 10 lines in a single file
+
+| Files 10000  | Files 1000 | Files 100 |    Files 10 | function  |
+|:-------------|:----------:|----------:|------------:|----------:|
+| 9 sec 478 ms |   167 ms   |    152 ms |   86 ms     |getNumOfLines()           |
+| 1 sec 733 ms |   228 ms   |     61 ms |   47 ms     |getNumOfLinesThreads()    |
+| 2 sec 90 ms  |   228 ms   |     55 ms |   47 ms     |getNumOfLinesThreadPool() |
--- src/src/Ex2/Ex2_1Test.java
+++ src/src/Ex2/Ex2_1Test.java
@@ -8,20 +8,27 @@ import static org.testng.AssertJUnit.*;
 
 class Ex2_1Test
 {
-    public static final int NUM_OF_FILES = 100;
+    public static final int NUM_OF_FILES_1 = 10;
+    public static final int NUM_OF_FILES_2 = 100;
+    public static final int NUM_OF_FILES_3 = 1000;
+    public static final int NUM_OF_FILES_4 = 10000;
+
+    public static final int BOUND_1 = 10;
+    public static final int BOUND_2 = 100;
+    public static final int BOUND_3 = 1000;
 
     @Test
     void createTextFiles()
     {
-        Ex2_1.createTextFiles(NUM_OF_FILES, 1, 1000);
+        Ex2_1.createTextFiles(NUM_OF_FILES_4, 1, BOUND_3);
     }
 
     @Test
     void getNumOfLines()
     {
-        String[] filenames = new String[NUM_OF_FILES];
+        String[] filenames = new String[NUM_OF_FILES_4];
 
-        for(int i = 0; i < NUM_OF_FILES; i++)
+        for(int i = 0; i < NUM_OF_FILES_4; i++)
         {
             filenames[i] = "text_files\\file_" + (i+1) + ".txt";
         }
@@ -33,9 +40,9 @@ class Ex2_1Test
     @Test
     void getNumOfLinesThreads()
     {
-        String[] filenames = new String[NUM_OF_FILES];
+        String[] filenames = new String[NUM_OF_FILES_4];
 
-        for(int i = 0; i < NUM_OF_FILES; i++)
+        for(int i = 0; i < NUM_OF_FILES_4; i++)
         {
             filenames[i] = "text_files\\file_" + (i+1) + ".txt";
         }
@@ -46,9 +53,9 @@ class Ex2_1Test
     @Test
     void getNumOfLinesThreadPool()
     {
-        String[] filenames = new String[NUM_OF_FILES];
+        String[] filenames = new String[NUM_OF_FILES_4];
 
-        for(int i = 0; i < NUM_OF_FILES; i++)
+        for(int i = 0; i < NUM_OF_FILES_4; i++)
         {
             filenames[i] = "text_files\\file_" + (i+1) + ".txt";
         }
