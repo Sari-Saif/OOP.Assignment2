8652759a4cc4ab57f59f44398726f11ffc26d5ce
SimchaTeich <sim.teich@gmail.com>
2023-01-03 20:31:15 +0200

    update tests

--- src/src/Ex2/Ex2_1Test.java
+++ src/src/Ex2/Ex2_1Test.java
@@ -6,18 +6,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class Ex2_1Test
 {
-
-    String[] filenames;
-
     @Test
     void createTextFiles()
     {
-        filenames = Ex2_1.createTextFiles(5, 2, 5);
+        Ex2_1.createTextFiles(5, 1, 10);
     }
 
     @Test
     void getNumOfLines()
     {
+        String[] filenames = {"test_me1.txt", "test_me2.txt", "test_me3.txt"};
         System.out.println("number of lines of all files: " + Ex2_1.getNumOfLines(filenames));
     }
 }
