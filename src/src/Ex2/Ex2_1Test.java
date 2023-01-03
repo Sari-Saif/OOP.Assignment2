8a69ba133f1173692baf6443ab35f22b45442ceb
SimchaTeich <sim.teich@gmail.com>
2023-01-03 23:03:47 +0200

    build simple basic test for the last function

--- src/src/Ex2/Ex2_1Test.java
+++ src/src/Ex2/Ex2_1Test.java
@@ -42,4 +42,17 @@ class Ex2_1Test
 
         System.out.println(Ex2_1.getNumOfLinesThreads(filenames));
     }
+
+    @Test
+    void getNumOfLinesThreadPool()
+    {
+        String[] filenames = new String[NUM_OF_FILES];
+
+        for(int i = 0; i < NUM_OF_FILES; i++)
+        {
+            filenames[i] = "text_files\\file_" + (i+1) + ".txt";
+        }
+
+        System.out.println(Ex2_1.getNumOfLinesThreadPool(filenames));
+    }
 }
