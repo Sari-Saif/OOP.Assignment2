f9ea4665b353166c8be69b322b6645612a11917a
SimchaTeich <sim.teich@gmail.com>
2023-01-05 14:20:58 +0200

    play with enum

--- src/src/Ex2_2/Main.java
+++ src/src/Ex2_2/Main.java
@@ -1,4 +1,26 @@
 package Ex2_2;
 
-public class Main {
+public class Main
+{
+    public static void main(String[] args)
+    {
+        TaskType tt1 = TaskType.COMPUTATIONAL;
+        System.out.println(tt1);
+
+        TaskType tt2 = TaskType.IO;
+        System.out.println(tt2);
+
+        TaskType tt3 = TaskType.OTHER;
+        System.out.println(tt3);
+
+        System.out.println(tt1.getPriorityValue());
+        System.out.println(tt2.getPriorityValue());
+        System.out.println(tt3.getPriorityValue());
+
+        tt1.setPriority(9);
+        System.out.println(tt1.getPriorityValue());
+        System.out.println(tt1);
+        System.out.println(tt1.getType());
+
+    }
 }
