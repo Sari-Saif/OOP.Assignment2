cf58ba78ea213e06e2d21027c7f9f681c1572c1b
SimchaTeich <sim.teich@gmail.com>
2023-01-06 01:03:20 +0200

    implement CustomExecutor

--- src/src/Ex2_2/CustomExecutor.java
+++ src/src/Ex2_2/CustomExecutor.java
@@ -1,4 +1,48 @@
 package Ex2_2;
 
-public class CustomExecutor {
+import java.util.concurrent.Callable;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class CustomExecutor
+{
+    private ThreadPoolExecutor threadPool;
+
+    CustomExecutor()
+    {
+        int numOfCores = Runtime.getRuntime().availableProcessors();
+        this.threadPool = new ThreadPoolExecutor(numOfCores/2,
+                numOfCores-1,
+                300,
+                TimeUnit.MILLISECONDS,
+                new PriorityBlockingQueue<>());
+    }
+
+
+    public Object submit(Task task)
+    {
+        return this.threadPool.submit(task);
+    }
+
+
+    public Object submit(Callable c, TaskType tt)
+    {
+        return submit(Task.createTask(c, tt));
+    }
+
+
+    public Object submit(Callable c)
+    {
+        return submit(Task.createTask(c));
+    }
+
+
+    // TODO: public static TaskType getCurrentMax()
+
+
+    public void gracefullyTerminate()
+    {
+        this.threadPool.shutdown();
+    }
 }
