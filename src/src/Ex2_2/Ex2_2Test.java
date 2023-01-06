4343d66ca4de3c27b8e95e55965e0c67364cfedf
SimchaTeich <sim.teich@gmail.com>
2023-01-06 13:25:11 +0200

    test like instructions

--- src/src/Ex2_2/Ex2_2Test.java
+++ src/src/Ex2_2/Ex2_2Test.java
@@ -8,11 +8,10 @@ import java.util.concurrent.*;
 public class Ex2_2Test
 {
     public static final Logger logger = LoggerFactory.getLogger(Ex2_2Test.class);
-    @Test
-    public void partialTest()
-    {
-        CustomExecutor customExecutor = new CustomExecutor();
 
+    @Test
+    public void partialTest(){
+        CustomExecutor customExecutor = new CustomExecutor();
         Task<Integer> task = Task.createTask(()->{
             int sum = 0;
             for (int i = 1; i <= 10; i++) {
@@ -20,35 +19,25 @@ public class Ex2_2Test
             }
             return sum;
         }, TaskType.COMPUTATIONAL);
-
         Future<Integer> sumTask = customExecutor.submit(task);
-
         final int sum;
-
         try {
             sum = sumTask.get(1, TimeUnit.MILLISECONDS);
         } catch (InterruptedException | ExecutionException | TimeoutException e) {
             throw new RuntimeException(e);
         }
         logger.info(()-> "Sum of 1 through 10 = " + sum);
-
-
         Callable<Double> callable1 = ()-> {
             return 1000 * Math.pow(1.02, 5);
         };
-
-
         Callable<String> callable2 = ()-> {
             StringBuilder sb = new StringBuilder("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
             return sb.reverse().toString();
         };
-
-
         // var is used to infer the declared type automatically
         Future<Double> priceTask = customExecutor.submit(()-> {
             return 1000 * Math.pow(1.02, 5);
         }, TaskType.COMPUTATIONAL);
-
         Future<String> reverseTask = customExecutor.submit(callable2, TaskType.IO);
         final Double totalPrice;
         final String reversed;
@@ -61,8 +50,9 @@ public class Ex2_2Test
         logger.info(()-> "Reversed String = " + reversed);
         logger.info(()->String.valueOf("Total Price = " + totalPrice));
         //logger.info(()-> "Current maximum priority = " +
-        //customExecutor.getCurrentMax());
+        //        customExecutor.getCurrentMax());
         customExecutor.gracefullyTerminate();
     }
+
 
 }
