116b924970eae651f5d5a559e8e54346d31b0f3b
SimchaTeich <sim.teich@gmail.com>
2023-01-09 01:44:25 +0200

    document function run

--- src/src/Ex2_1/GetNumOfLinesThread.java
+++ src/src/Ex2_1/GetNumOfLinesThread.java
@@ -16,6 +16,7 @@ public class GetNumOfLinesThread extends Thread
         this.filename = fileName;
     }
 
+
     /**
      * function getter
      * @return the number of lines class member.
@@ -25,11 +26,19 @@ public class GetNumOfLinesThread extends Thread
         return this.numberOfLines;
     }
 
+
+    /**
+     * function run override run of Thread.
+     * The goal is to save the result inside
+     * class member, because run() can't returns value.
+     */
     @Override
     public void run()
     {
         String[] arrWithOneFilename = new String[1];
         arrWithOneFilename[0] = this.filename;
+
+        // using function for counting number of lines was written before.
         numberOfLines = Ex2_1.getNumOfLines(arrWithOneFilename);
     }
 }
