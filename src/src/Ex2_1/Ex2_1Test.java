c624a74c93da138acb84c81454e409fd2487584a
SimchaTeich <sim.teich@gmail.com>
2023-01-24 08:04:23 +0200

    fix what was change in the exam to what was before

--- src/src/Ex2_1/Ex2_1Test.java
+++ src/src/Ex2_1/Ex2_1Test.java
@@ -9,7 +9,6 @@ class Ex2_1Test
     public static final int NUM_OF_FILES_2 = 100;
     public static final int NUM_OF_FILES_3 = 1000;
     public static final int NUM_OF_FILES_4 = 10000;
-    public static final int NUM_OF_FILES_5 = 100000;
 
     public static final int BOUND_1 = 10;
     public static final int BOUND_2 = 100;
@@ -18,49 +17,47 @@ class Ex2_1Test
     @Test
     void createTextFiles()
     {
-        Ex2_1.createTextFiles(NUM_OF_FILES_5, 1, BOUND_2);
+        Ex2_1.createTextFiles(NUM_OF_FILES_1, 1, BOUND_1);
     }
 
     @Test
     void getNumOfLines()
     {
-        String[] filenames = new String[NUM_OF_FILES_5];
+        String[] filenames = new String[NUM_OF_FILES_1];
         Ex2_1 threadsUsing = new Ex2_1();
-        for(int i = 0; i < NUM_OF_FILES_5; i++)
+        for(int i = 0; i < NUM_OF_FILES_1; i++)
         {
-            filenames[i] = "text_files\\file_" + (i+1) + ".txt";
+            filenames[i] = "files_for_test\\file_" + (i+1) + ".txt";
         }
-       //assertEquals (57,threadsUsing.getNumOfLinesThreads(filenames));
-        System.out.println(threadsUsing.getNumOfLinesThreads(filenames));
+        assertEquals (57,threadsUsing.getNumOfLinesThreads(filenames));
+
     }
 
     @Test
     void getNumOfLinesThreads()
     {
         Ex2_1 threadsUsing = new Ex2_1();
-        String[] filenames = new String[NUM_OF_FILES_5];
+        String[] filenames = new String[NUM_OF_FILES_1];
 
-        for(int i = 0; i < NUM_OF_FILES_5; i++)
+        for(int i = 0; i < NUM_OF_FILES_1; i++)
         {
-            filenames[i] = "text_files\\file_" + (i+1) + ".txt";
+            filenames[i] = "files_for_test\\file_" + (i+1) + ".txt";
         }
-        //assertEquals(57,threadsUsing.getNumOfLinesThreads(filenames));
-        System.out.println(threadsUsing.getNumOfLinesThreads(filenames));
+        assertEquals(57,threadsUsing.getNumOfLinesThreads(filenames));
     }
 
     @Test
     void getNumOfLinesThreadPool()
     {
         Ex2_1 threadPoolUsing = new Ex2_1();
-        String[] filenames = new String[NUM_OF_FILES_5];
+        String[] filenames = new String[NUM_OF_FILES_1];
 
-        for(int i = 0; i < NUM_OF_FILES_5; i++)
+        for(int i = 0; i < NUM_OF_FILES_1; i++)
         {
-            filenames[i] = "text_files\\file_" + (i+1) + ".txt";
+            filenames[i] = "files_for_test\\file_" + (i+1) + ".txt";
         }
 
-        //assertEquals(57,threadPoolUsing.getNumOfLinesThreadPool(filenames));
-        System.out.println(threadPoolUsing.getNumOfLinesThreadPool(filenames));
+        assertEquals(57,threadPoolUsing.getNumOfLinesThreadPool(filenames));
 
     }
 }
